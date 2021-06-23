c886979c8edecb64c483c3ab0a0e3fbb6ccfefa9
Luis Augusto Toscano <dev.ltoscano@gmail.com>
2021-06-22 23:11:18 -0300

    Changed bed leveling grid size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1398,7 +1398,7 @@
  * these options to restore the prior leveling state or to always enable
  * leveling immediately after G28.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28
 //#define ENABLE_LEVELING_AFTER_G28
 
 /**
@@ -1456,7 +1456,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
